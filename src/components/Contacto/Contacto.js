11ed746d68e70549f0cdb597f5be31292ac5c16d
Franco1012 <solerfrancogerman@gmail.com>
2024-09-12 15:32:32 -0300

    se corrige posición del pie de página

--- src/components/Contacto/Contacto.js
+++ src/components/Contacto/Contacto.js
@@ -1,8 +1,21 @@
 import React from 'react'
+import styled from 'styled-components';
+// Definir el estilo para el contenedor y el título usando styled-components
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90vh;
+  background-color: #f5f5f5;
+`;
 
+const Title = styled.h1`
+  color: #333;
+  font-size: 2.5rem;
+`;
 const Contacto = () => {
   return (
-    <div><h1 style={{display:'flex', justifyContent:'center'}}>Contacto</h1></div>
+    <Container><Title>Contacto</Title></Container>
   )
 }
 export default Contacto
